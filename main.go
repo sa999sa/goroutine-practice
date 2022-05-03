c9ef5ccf34b828d0aa2312c97492f1754355ca90
sa999na <lumimosita999@icloud.com>
2022-05-03 23:04:35 +0900

    goroutine生成前後での確保されたメモリ量を計算

--- main.go
+++ main.go
@@ -2,19 +2,33 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
 )
 
 func main() {
-	var wg sync.WaitGroup
-	sayHello := func() {
-		defer wg.Done()
-		fmt.Println("hello")
+
+	memConsumed := func() uint64 {
+		runtime.GC()
+		var s runtime.MemStats
+		runtime.ReadMemStats(&s)
+		return s.Sys
+
 	}
-	wg.Add(1)
-	go sayHello()
+
+	var c <-chan interface{}
+	var wg sync.WaitGroup
+	noop := func() { wg.Done(); <-c }
+	const numGoroutines = 1e4
+	wg.Add(numGoroutines)
+	before := memConsumed()
+	for i := numGoroutines; i > 0; i-- {
+		go noop()
+	}
+
 	wg.Wait()
-	//fmt.Println("")
+	after := memConsumed()
+	fmt.Printf("%.3fkb", float64(after-before)/numGoroutines/1000)
 }
 
 /*func sayHello() {
