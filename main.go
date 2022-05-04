1f963460748941791d596693e8b72e0467fd16d7
sa999na <lumimosita999@icloud.com>
2022-05-04 14:37:56 +0900

    queue

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 func main() {
-	fmt.Println("test")
+	c := sync.NewCond(&sync.Mutex{})
+	queue := make([]interface{}, 0, 10)
+	removeFromQueue := func(delay time.Duration) {
+		time.Sleep(delay)
+		c.L.Lock()
+		queue = queue[1:]
+		fmt.Println("Remove from queue")
+		c.L.Unlock()
+		c.Signal()
+	}
+	for i := 0; i < 10; i++ {
+		c.L.Lock()
+		for len(queue) == 2 {
+			c.Wait()
+		}
+	}
+	fmt.Println("Adding to queue")
+	queue = append(queue, struct{}{})
+	go removeFromQueue(1 * time.Second)
+	c.L.Unlock()
+	// time.Sleep(delay)
+	//fmt.Println("")
 }
+
+/*func sayHello() {
+	fmt.Println("Hello")
+}*/
